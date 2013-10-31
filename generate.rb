59797c83e298d7cbe021ec915317f70ee7a9ab98
Adam Wong <adamwong246@gmail.com>
2013-10-31 12:25:16 -0700

    update

--- generate.rb
+++ generate.rb
@@ -3,7 +3,7 @@ require 'erb'
 require 'yaml'
  
 # renders erb to string
-def render(file)
+def render_file(file)
 	content = File.read("#{file}")
 	template = ERB.new(content).result	
 end
@@ -13,6 +13,9 @@ config = YAML.load_file("config.yml")
 
 # recursively process each file
 Dir.glob("templates/**/*", File::FNM_DOTMATCH) do |file| # note one extra "*"
-  puts "working on: #{file}..."
-  File.open("home/#{File.basename(file, ".erb")}", 'w') { |f| f.puts(render(file)) }  
+	puts "reading #{file}..."  
+  dest = "home/#{File.basename(file, ".erb")}"
+  File.open(dest, 'w') { |f| f.puts(render_file(file)) }  
+  puts "written to #{dest}."
+
 end
--- home/.vimrc
+++ home/.vimrc
@@ -1,5 +1,6 @@
-"Made with love, by Adam Wong
-
+"Shuri, Adam Wong's castle.
+"2013-10-31T12:24:55-07:00
+"This file was generated from generate.rb. It can be found here: https://github.com/adamwong246/shuri. It was made to work with homesick.
 
 " Turn on line number
 :set number
