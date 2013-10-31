251d28f6df1ebb302879c25246568e2d0da7871f
Adam Wong <adamwong246@gmail.com>
2013-10-31 11:41:04 -0700

    wip, script works

--- generate.rb
+++ generate.rb
@@ -1,11 +1,19 @@
 require 'rubygems'
 require 'erb'
  
-# setup - could be initialized from script arguments
-classname = "Contact"
-listener_methods = ["added", "removed", "blackListed"]
+
+Dir.glob("templates/**/*", File::FNM_DOTMATCH) do |my_text_file| # note one extra "*"
+  puts "working on: #{my_text_file}..."
+  content = File.read("#{my_text_file}")
+	template = ERB.new(content)
+	File.open("home/#{File.basename(my_text_file)}", 'w') { |f| f.puts(template.result) }
+end
+
+# # setup - could be initialized from script arguments
+# classname = "Contact"
+# listener_methods = ["added", "removed", "blackListed"]
  
-# create file based on 'listener.java.erb'
-content = File.read('listener.java.erb')
-template = ERB.new(content)
-File.open("#{classname}Listener.java", 'w') { |f| f.puts(template.result) }
+# # create file based on 'listener.java.erb'
+# content = File.read('listener.java.erb')
+# template = ERB.new(content)
+# File.open("#{classname}Listener.java", 'w') { |f| f.puts(template.result) }
