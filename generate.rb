b717148ea6ca47a2a814e6d3343ceab7bb886cb6
Adam Wong <adamwong246@gmail.com>
2013-11-01 22:45:18 -0700

    whatever

--- generate.rb
+++ generate.rb
@@ -7,6 +7,18 @@ require 'ostruct'
 require 'fileutils'
 require 'erubis'
 
+class Hash
+  def method_missing(method, *opts)
+    m = method.to_s
+    if self.has_key?(m)
+      return self[m]
+    elsif self.has_key?(m.to_sym)
+      return self[m.to_sym]
+    end
+    super
+  end
+end
+
 class Namespace
   def initialize(hash)
     hash.each do |key, value|
@@ -53,7 +65,7 @@ def component(color, component)
 	color.scan(/../).map {|color| color.to_i(16)}[map[component]]
 end
 
-def make_directories(src_dir, dest_dir)
+def make_directories(src_dir, dest_dir, opts)
   Dir.glob("#{src_dir}/**/*/", File::FNM_DOTMATCH) do |file|
     puts "making directory: #{file}"
     new_dest = "#{dest_dir}/" + file.sub("#{src_dir}/", "")
@@ -63,6 +75,7 @@ def make_directories(src_dir, dest_dir)
 end
 
 def render_file(file, opts={})
+  puts "rendering #{file} with #{opts}"
   render_template(File.new(file).read, opts)
 end
 
@@ -71,35 +84,37 @@ def render_template(template, opts={})
   ERB.new(template).result(ns.get_binding)
 end
 
-def make_files(src_dir, dest_dir, &block)
+def make_files(src_dir, dest_dir, opts)
   Dir.glob("#{src_dir}/**/**", File::FNM_DOTMATCH) do |file|
 
     if File.file?(file)
     
-      result = render_file(file, @@hash)
+      result = render_file(file, opts)
 
       dest_dir = "home"
       src_dir = "templates"
 
       string_to_eval = dest_dir + file.sub(src_dir, '')
-      puts string_to_eval
 
-      new_dest = render_template(string_to_eval,@@hash ).chomp(File.extname(file))      
+      debugger
+      final_file = render_template(string_to_eval, @@hash)  # Proc.new { |var| eval(%Q{"#{string_to_eval}"})  }.call 
+      
+      new_dest = render_template(final_file, opts ).chomp(File.extname(file))      
 
       File.open(new_dest, 'w') { |f| f.puts(result) }      
     end
   end
 end
 
-def proccess_files(src_dir, dest_dir, &block)
-  make_directories(src_dir, dest_dir)
-  make_files(src_dir, dest_dir)  
+def proccess_files(src_dir, dest_dir, opts)
+  make_directories(src_dir, dest_dir, opts)
+  make_files(src_dir, dest_dir, opts)  
 end
 
 ##### START HERE
 @@hash = YAML.load_file("config.yml")
 # @conf = DeepStruct.new(YAML.load_file("config.yml"))
 
-proccess_files("templates", "home") 
+proccess_files("templates", "home", @@hash) 
 
 puts "EXIT CODE 0"
